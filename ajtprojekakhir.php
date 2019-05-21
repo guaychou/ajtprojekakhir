e58392c26603deefc3a6da2779bb52e9cdceb904
Kevin Jonathan Harnanta <crossmajor99@gmail.com>
2019-05-21 16:24:30 +0700

    Create file ajtprojekakhir
    This is the dashboard to showing the data from database. The data from database is inserted by webservice.php .

---
+++ ajtprojekakhir.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<table>
+  <tr>
+    <th>Suhu</th>
+    <th>Kelembapan</th>
+    <th>Class Suhu</th>
+    <th>Class kelembapan</th>
+  </tr>
+<?php
+$servername = "localhost";
+$username = "CHANGE ME";
+$password = "CHANGE ME";
+$dbname = "CHANGE ME";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT *  FROM ajtprojekakhir" ;
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+       // echo "Suhu: " . $row["suhu"]. " - Kelembapan: " . $row["kelembapan"]. "- Class Suhu " . $row["classSuhu"]. " - ".$row["classKelembapan"]." <br> ";
+	echo "<tr>";
+        echo "<td>".$row["suhu"]."</td>";
+	echo "<td>".$row["kelembapan"]."</td>";
+	echo "<td>".$row["classSuhu"]."</td>";
+	echo "<td>".$row["classKelembapan"]."</td>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+ </tr>
+</table>
+
+</body>
+</html>
